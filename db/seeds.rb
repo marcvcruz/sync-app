edb5f22fed6ac958b2970804d5c68cf7c9e74ef4
Marc Bell-Cruz <marc.cruz@gettyimages.com>
2015-02-27 13:42:09 -0800

    Seeding users table with god

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+User.create(first_name:'God', last_name: 'Almighty', email: 'marcvcruz@hotmail.com', username: 'god', password:'My name is God almighty.')
