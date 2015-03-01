9ab6ecc68f7cd1ed6ff2389293c7895b56a4810d
Marc Bell-Cruz <marc.cruz@gettyimages.com>
2015-02-28 23:35:39 -0800

    Adding before filter to check if user is signed in

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,16 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
+  before_filter :authenticate
+
   def current_user
     @current_user ||= User.find(session[:user_id]) unless session[:user_id].nil?
   end
+
+  private
+
+  def authenticate
+    redirect_to :sign_in if current_user.nil?
+  end
+
 end
--- app/controllers/session_controller.rb
+++ app/controllers/session_controller.rb
@@ -1,4 +1,6 @@
 class SessionController < ApplicationController
+  skip_before_filter :authenticate, except: :destroy
+
   def new
   end
 
