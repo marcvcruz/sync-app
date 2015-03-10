44a817f73e798d69dcf6d81f33cde510b0e437b2
Marc Bell-Cruz <marc.cruz@gettyimages.com>
2015-03-10 00:07:09 -0700

    Adding tests for session helper

--- spec/helpers/session_helper_spec.rb
+++ spec/helpers/session_helper_spec.rb
@@ -11,5 +11,26 @@ require 'rails_helper'
 #   end
 # end
 RSpec.describe SessionHelper, type: :helper do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  let(:user) {
+    User.first
+  }
+
+  it 'can sign in a user' do
+    sign_in user
+    expect(current_user).to_not be_nil
+    expect(cookies.signed[:user_id]).to be user.id
+  end
+
+  it 'can remember a user' do
+    remember user
+    expect(cookies.signed[:remember_token]).to eq user.remember_token
+  end
+
+  it 'can sign out and forget a user' do
+    sign_out
+    expect(current_user).to be_nil
+    expect(cookies.signed[:user_id]).to be_nil
+    expect(cookies.signed[:remember_token]).to be_nil
+  end
 end
