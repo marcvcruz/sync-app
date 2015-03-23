74d44d81e4f3244d1414c294218db60c92ddf1cf
Marc Bell-Cruz <marc.cruz@gettyimages.com>
2015-03-23 00:19:44 -0700

    Implemented the create, edit and update actions of UsersController

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,6 +12,27 @@ class UsersController < ApplicationController
   end
 
   def create
-
+    @user = User.new(create_user_params)
+    redirect_to users_path and return if @user.save
+    render :new
   end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find params[:id]
+    redirect_to users_path and return if @user.update update_user_params
+    render :edit
+  end
+  private
+
+    def create_user_params
+      params.require(:user).permit!
+    end
+
+    def update_user_params
+      params.require(:user).permit(:first_name, :last_name, :email, :is_admin)
+    end
 end
