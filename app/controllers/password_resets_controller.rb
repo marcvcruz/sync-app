174a564b31f35266b55867d66e7c756eea55af82
Marc Bell-Cruz <marc.cruz@gettyimages.com>
2015-03-27 01:00:13 -0700

    Controller, views and helper files for password reset

---
+++ app/controllers/password_resets_controller.rb
@@ -0,0 +1,16 @@
+class PasswordResetsController < ApplicationController
+  skip_before_filter :authenticate
+
+  def new
+  end
+
+  def create
+
+  end
+
+  def edit
+  end
+
+  def patch
+  end
+end
---
+++ spec/controllers/password_resets_controller_spec.rb
@@ -0,0 +1,34 @@
+require 'rails_helper'
+
+RSpec.describe PasswordResetsController, type: :controller do
+
+  describe '#new' do
+    it 'returns http success' do
+      get :new
+      expect(response).to render_template :new
+      expect(response).to have_http_status :success
+    end
+  end
+
+  describe '#create' do
+    it 'returns http success' do
+      get :create
+      expect(response).to have_http_status :success
+    end
+  end
+
+  describe '#edit' do
+    it 'returns http success' do
+      get :edit
+      expect(response).to have_http_status :success
+    end
+  end
+
+  describe '#patch' do
+    it 'returns http success' do
+      get :patch
+      expect(response).to have_http_status :success
+    end
+  end
+
+end
