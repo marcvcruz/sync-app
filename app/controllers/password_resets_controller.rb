7f79221b0bbfebdbf47b55013497aabf61cbfc47
Marc Bell-Cruz <marc.cruz@gettyimages.com>
2015-04-01 01:48:47 -0700

    Refactor user retrieval, user validation and checking of expiration to before filters

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -1,5 +1,8 @@
 class PasswordResetsController < ApplicationController
   skip_before_filter :authenticate
+  before_filter :get_user, only: [:edit, :update]
+  before_filter :validate_user, only: [:edit, :update]
+  before_filter :check_expiration, only: [:edit, :update]
 
   def new
   end
@@ -9,8 +12,6 @@ class PasswordResetsController < ApplicationController
     if @user
       @user.create_reset_digest
       @user.send_password_reset_email
-      flash[:info] = t :email_sent_with_password_reset_instructions
-
     end
   end
 
@@ -18,5 +19,22 @@ class PasswordResetsController < ApplicationController
   end
 
   def patch
+  private
+
+  def get_user
+    @user = User.find_by_email params[:email]
+  end
+
+  def validate_user
+    unless @user and @user.authenticated? :reset, params[:token]
+      redirect_to new_password_reset_url
+    end
+  end
+
+  def check_expiration
+    if @user.reset_sent_at.advance(hours: 24).past?
+      flash[:alert] = t :password_reset_token_expired
+      redirect_to new_password_reset_url
+    end
   end
 end
