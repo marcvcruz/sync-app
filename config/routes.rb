b6442b5a33d84a97aef1dbde373c60cfeb4bc1fd
Marc Bell-Cruz <marc.cruz@gettyimages.com>
2015-04-01 02:00:03 -0700

    Modified path to match for password resets

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
 
   match '/forgot-password' => 'password_resets#new', via: :get, as: :new_password_reset
   match '/forgot-password' => 'password_resets#create', via: :post, as: :create_password_reset
-  match '/password-reset/:token' => 'password_resets#edit', via: :get, as: :edit_password_reset
+  match '/password-resets/:token' => 'password_resets#edit', via: :get, as: :edit_password_reset
   match '/password-resets/:token' => 'password_resets#update', via: :put, as: :update_password_reset
 
   match '/sign-in' => 'session#new', via: :get, as: :sign_in
