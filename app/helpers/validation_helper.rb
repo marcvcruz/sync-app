83b56f7d429d3873bc36a983d6e9f9fd0dffa1ac
Marc Bell-Cruz <marc.cruz@gettyimages.com>
2015-04-01 16:58:06 -0700

    Modified the helper module that checks for and builds validation error messages to be usable for all models

---
+++ app/helpers/validation_helper.rb
@@ -0,0 +1,10 @@
+module ValidationHelper
+  def error_exists?(model, field)
+    error_messages = model.errors.messages
+    error_messages.has_key? field and not error_messages[field].empty?
+  end
+  
+  def error_message_for(model, field)
+    field.to_s.humanize << ' ' << (model.errors.messages[field][0] || '')
+  end
+end
