4742643af5761f0a0409a6d63b6d49b871d1aabb
Marc Bell-Cruz <marc.cruz@gettyimages.com>
2015-04-07 05:19:17 -0700

    Completed unit tests for user mailer, validation helper and password resets controller

--- spec/controllers/password_resets_controller_spec.rb
+++ spec/controllers/password_resets_controller_spec.rb
@@ -2,6 +2,8 @@ require 'rails_helper'
 
 RSpec.describe PasswordResetsController, type: :controller do
 
+  let(:user) { FactoryGirl.create :user }
+
   describe '#new' do
     it 'returns http success' do
       get :new
@@ -12,23 +14,48 @@ RSpec.describe PasswordResetsController, type: :controller do
 
   describe '#create' do
     it 'returns http success' do
-      get :create
+      post :create, password_reset: { username: user.username }
       expect(response).to have_http_status :success
+      expect(assigns(:user)).to eql user
     end
   end
 
   describe '#edit' do
-    it 'returns http success' do
-      user = FactoryGirl.create
-      get :edit
-      expect(response).to have_http_status :success
+    before :each do
+      user.create_reset_digest
+    end
+
+    context 'with a valid reset token' do
+      it 'returns http success' do
+        get :edit, token: user.reset_token, username: user.username
+        expect(response).to have_http_status :success
+      end
+    end
+
+    context 'with an expired reset token' do
+      before :each do
+        user.update_attribute :reset_sent_at, 5.days.ago
+      end
+
+      it 'redirects to //forgot-password' do
+        get :edit, token: user.reset_token, username: user.username
+        expect(response).to redirect_to new_password_reset_url
+      end
     end
   end
 
-  describe '#patch' do
-    it 'returns http success' do
-      get :patch
-      expect(response).to have_http_status :success
+  describe '#update' do
+    before :each do
+      user.create_reset_digest
+    end
+
+    it 'redirects to homepage' do
+      old_password = user.password
+      put :update, token: user.reset_token, username: user.username, user: { password: 'New Password', password_confirmation: 'New Password' }
+      expect(controller.current_user).to eql user
+      expect(controller.current_user.authenticated?(:password, old_password)).to be false
+      expect(controller.current_user.authenticated?(:password, 'New Password')).to be true
+      expect(response).to redirect_to root_path
     end
   end
 
--- spec/mailers/user_mailer_spec.rb
+++ spec/mailers/user_mailer_spec.rb
@@ -1,17 +1,21 @@
-require "rails_helper"
+require 'rails_helper'
 
 RSpec.describe UserMailer, type: :mailer do
-  describe "password_reset" do
-    let(:mail) { UserMailer.password_reset }
+  describe 'password_reset' do
+    let(:user) { FactoryGirl.create :user }
+    let(:mail) {
+      user.create_reset_digest
+      UserMailer.password_reset user
+    }
 
-    it "renders the headers" do
-      expect(mail.subject).to eq("Password reset")
-      expect(mail.to).to eq(["to@example.org"])
-      expect(mail.from).to eq(["from@example.com"])
+    it 'renders the headers' do
+      expect(mail.subject).to eq('Password reset')
+      expect(mail.to).to eq([user.email])
+      expect(mail.from).to eq(['no-reply@sync-app.com'])
     end
 
-    it "renders the body" do
-      expect(mail.body.encoded).to match("Hi")
+    it 'renders the body' do
+      expect(mail.body.encoded).to match /[a-z]*[0-9]*password-resets[a-z]*[0-9]*/i
     end
   end
 
