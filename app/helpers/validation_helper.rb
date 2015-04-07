7942c98f19ba7efc024961bd844e193c114987be
Marc Bell-Cruz <marc.cruz@gettyimages.com>
2015-04-07 05:17:17 -0700

    Updates to validation helper and

--- app/helpers/validation_helper.rb
+++ app/helpers/validation_helper.rb
@@ -1,10 +1,11 @@
 module ValidationHelper
   def error_exists?(model, field)
+    return false if model.valid?
     error_messages = model.errors.messages
     error_messages.has_key? field and not error_messages[field].empty?
   end
   
   def error_message_for(model, field)
-    field.to_s.humanize << ' ' << (model.errors.messages[field][0] || '')
+    field.to_s.humanize << ' ' << (model.errors.messages[field][0] || '') if error_exists? model, field
   end
 end
