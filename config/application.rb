d48fa98abfa22c7051720cfd5b199ca17c81753b
Marc Cruz <marcvcruz@hotmail.com>
2015-04-07 23:14:34 -0700

    Time zone and calendar configuration

--- config/application.rb
+++ config/application.rb
@@ -20,9 +20,7 @@ module SyncApp
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
 
-    # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
-    # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
-    # config.time_zone = 'Central Time (US & Canada)'
+    config.time_zone = 'Pacific Time (US & Canada)'
 
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
@@ -42,5 +40,7 @@ module SyncApp
       g.fixture_replacement :factory_girl,
                             :dir => "spec/factories"
     end
+
+    config.beginning_of_week = :sunday
   end
 end
