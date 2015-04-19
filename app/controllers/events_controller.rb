cd7d98ca90fe435efe33529d806bf601b55dc1e8
Marc Bell-Cruz <marc.cruz@gettyimages.com>
2015-04-19 02:26:34 -0700

    Refactored EventsController

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,15 +1,17 @@
 class EventsController < ApplicationController
-  before_filter :get_event, only: [:new, :create]
+  before_filter :get_event, only: [:edit, :update]
 
   def index
     @events = Event.all
   end
 
   def new
+    @event = Event.new
   end
 
   def create
-    if @event.save
+    @event = Event.new event_params.merge!(:organizer_id, current_user.id)
+    if @event.valid? and @event.save
       flash.now[:notice] = t :event_successfully_created
       redirect_to :events and return
     end
@@ -21,15 +23,21 @@ class EventsController < ApplicationController
   end
 
   def update
+    if @event.update_attributes event_params
+      flash.now[:notice] = t :event_successfully_updated
+      redirect_to :events and return
+    end
+    flash.now[:alert] = t :error_occurred_processing_last_request
+    render :edit
   end
 
   private
 
   def event_params
-    params.fetch(:event, {}).permit!
+    params.require(:event).permit(:description, :is_all_day, :starting_at, :notes)
   end
 
   def get_event
-    @event = Event.new event_params.reverse_merge!(organizer_id: current_user.id)
+    @event = Event.find params[:id]
   end
 end
