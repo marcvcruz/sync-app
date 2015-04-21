09778a265936cc5fb143b8b473a87af0f55640b0
Marc Bell-Cruz <marc.cruz@gettyimages.com>
2015-04-21 06:06:27 -0700

    Updated tests as a result of db schema changes

--- spec/controllers/events_controller_spec.rb
+++ spec/controllers/events_controller_spec.rb
@@ -24,25 +24,34 @@ RSpec.describe EventsController, type: :controller do
   end
 
   describe 'POST #create' do
-    xit 'returns http success' do
+    it 'returns http success' do
 
     end
   end
 
   describe 'GET #edit' do
-    xit 'returns http success' do
-      # get :edit
-      # expect(response).to have_http_status(:success)
-      # expect(response).to render_template :edit
-      # expect(assigns(:event))
+    let(:event) { FactoryGirl.create :event }
+
+    it 'is successful' do
+      get :edit, id: event.id
+      expect(response).to have_http_status(:success)
+      expect(response).to render_template :edit
+      expect(assigns(:event)).to eql event
     end
   end
 
   describe 'PUT #update' do
-    xit 'returns http success' do
-      # get :update
-      # expect(response).to have_http_status(:success)
-      # expect(response).to redirect_to :root
+    let(:event) { FactoryGirl.create :event }
+
+    it 'returns http success' do
+      get :update, id: event.id, event: { is_all_day: true, description: 'New description', notes: 'New note'  }
+      updated_event =  assigns(:event)
+      expect(updated_event).to_not be_nil
+      expect(updated_event.is_all_day).to eql true
+      expect(updated_event.description).to eql 'New description'
+      expect(updated_event.notes).to eql 'New note'
+      expect(response).to have_http_status(302)
+      expect(response).to redirect_to events_path
     end
   end
 
