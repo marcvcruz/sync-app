8154f8603a598fdfc6373f878a9fe64dc4e16b33
Marc Bell-Cruz <marc.cruz@gettyimages.com>
2015-05-06 22:51:24 -0700

    Fixes user retrieval when setting up the current user.

--- app/helpers/session_helper.rb
+++ app/helpers/session_helper.rb
@@ -18,7 +18,7 @@ module SessionHelper
 
   def current_user
     @current_user ||= if user_id = cookies.signed[:user_id]
-                        user = User.find_by(user_id)
+                        user = User.find_by id: user_id
                         user if user and user.authenticated? :remember, cookies.signed[:remember_token]
                       end
   end
